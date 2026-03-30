a20a69e1fd21992d97fa53978385feebe9642ca3
Veronika Pluzhnikova <vvvvvveronika@proton.me>
2026-03-30 08:40:01 +0200

    word animation

--- potato-script.js
+++ potato-script.js
@@ -1,5 +1,9 @@
 const html = document.documentElement;
 
+document.addEventListener('DOMContentLoaded', () => {
+  wordPlacement();
+});
+
 const savedTheme = localStorage.getItem("theme");
 if (savedTheme === "dark" || savedTheme === "light"){
     html.setAttribute("data-theme", savedTheme);
@@ -34,8 +38,25 @@ btn.addEventListener('click', () => {
 syncButtonLabel();
 
 
-//14:13 finds button -> when it is clicked -> runs instruction -> gets html, checks if the theme is dark and stores t/f -> updates the button (light/dark)//
-//15:56 updated version that includes the os settings//
+//finds button -> when it is clicked -> runs instruction -> gets html, checks if the theme is dark and stores t/f -> updates the button (light/dark)//
+
+function wordPlacement(){
+let allWords = document.querySelectorAll('.word');
+  let frameSize = [500, 300];
+  for (let i=0; i<allWords.length; i++){
+    const word = allWords[i];
+    const wordWidth = word.offsetWidth;
+    const wordHeight = word.offsetHeight;
+    let posX = Math.random()*(frameSize[0]-wordWidth);
+    let posY = Math.random()*(frameSize[1]-wordHeight);
+    word.style.left = posX + "px";
+    word.style.top = posY + "px";
+  }
+}
+
+
+
+
 
 const spotsLayer = document.createElement("div");
 spotsLayer.className = "greening-spots";
@@ -43,7 +64,7 @@ document.body.prepend(spotsLayer); //?//
 
 let spotsTimer = null;
 let spotsCount = 0;
-const maxSpots = 8;
+const maxSpots = 18;
 
 function rand(min, max) {
     return Math.random() * (max-min) + min; //?//
@@ -57,7 +78,7 @@ function addSpot() {
 
 const spot = document.createElement("div");
 spot.className = "greening-spot";
-spot.style.setProperty("--size", `${rand(60, 220)}px`); //what does $ stand for?//
+spot.style.setProperty("--size", `${rand(60, 220)}px`); 
 spot.style.setProperty("--x", `${rand(0,85)}vw`);
 spot.style.setProperty("--y", `${rand(0,85)}vh`);
 spot.style.setProperty("--radius", randomRadius());
@@ -68,7 +89,6 @@ spotsCount += 1;
 }
 
 function startGreeningSpots() {
-  stopGreeningSpots();
   addSpot();
   spotsTimer = setInterval(addSpot, 2500);
 }
@@ -86,3 +106,49 @@ function updateGreeningSpots() {
 }
 
 updateGreeningSpots();
+
+
+//make the section draggable:
+function dragElement(elmnt) {
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "framebody")) {
+    // if present, the header is where you move from:
+    document.getElementById(elmnt.id + "body").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move from anywhere inside:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+dragElement(document.getElementById("frame"));
+
