b01fbd04a4e6b0dbcd305cf739910164de2d9eb8
Veronika Pluzhnikova <vvvvvveronika@proton.me>
2026-05-07 17:22:40 +0200

    moved the js from the irl weggies.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const animateMove = (element, prop, pixels) =>
+  anime({
+    targets: element,
+    [prop]: `${pixels}px`,
+    easing: "easeOutCirc"
+  });
+
+const getRandomNumber = (num) => {
+  return Math.floor(Math.random() * (num + 1));
+};
+
+const runaway = (element) => {
+  const top = getRandomNumber(window.innerHeight - element.offsetHeight);
+  const left = getRandomNumber(window.innerWidth - element.offsetWidth);
+  animateMove(element, "left", left);
+  animateMove(element, "top", top);
+};
